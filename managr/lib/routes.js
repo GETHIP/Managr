e5c7bba3c523a02da59bb4419ab2fb035a4269fa
Gerot <Thomas_Gerot@gallup.com>
2016-06-08 16:35:19 -0500

    Routing is great!

--- managr/lib/routes.js
+++ managr/lib/routes.js
@@ -1,45 +1,45 @@
 FlowRouter.route("/", {
   name: "root",
   action() {
-    // TODO Display appropriate template
+    BlazeLayout.render("original",{content: "root"});
   }
 });
 FlowRouter.route("/assignments", {
   name: "all",
   action() {
-    // TODO Display the appropriate template
+    BlazeLayout.render("original",{content: "all"});
   }
 })
 /////////////////////////// TABLE OF ALL ASSIGNMENTS
 FlowRouter.route("/assignments/all", {
   name: "all",
   action() {
-    // TODO Display appropriate template
+    BlazeLayout.render("original",{content: "all"});
   }
 });
 FlowRouter.route("/assignments/edit/all", {
   name: "edit-all",
   action() {
-    // TODO Display appropriate template
+    BlazeLayout.render("original",{content: "edit-all"});
   }
 });
 /////////////////////////// SINGLE ASSIGNMENT INFO
 FlowRouter.route("/assignments/single/:id", { //TODO Add param
   name: "single", //TODO Add assignment number
   action(params) {
-    // TODO Display appropriate template
+    BlazeLayout.render("original",{content: "single"});
   }
 });
 FlowRouter.route("/assignments/edit/single/:id", {
   name: "edit-single",
   action(params) {
-    // TODO Display appropriate template
+    BlazeLayout.render("original",{content: "single-edit"});
   }
 });
 /////////////////////////// GRADE SPREADSHEET
 FlowRouter.route("/assignments/edit/grades", {
   name: "edit-grades",
   action() {
-    //TODO Display appropriate template
+    BlazeLayout.render("original",{content: "edit-grades"});
   }
 });
