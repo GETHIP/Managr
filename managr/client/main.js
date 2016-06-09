b5609fa9813d95ee003644863debe6840f2eca0f
Gerot <Thomas_Gerot@gallup.com>
2016-06-09 11:40:52 -0500

    Fixed remaining underscores

--- managr/client/main.js
+++ managr/client/main.js
@@ -1,18 +1,18 @@
 import { Template } from 'meteor/templating';
 Template.body.helpers({
   page: function() {
-    var current_page = current_page = FlowRouter.getRouteName();
+    var currentPage = currentPage = FlowRouter.getRouteName();
     if (current_page == null || current_page.length < 1) {
       return "404 Error. Page not found.";
     }
     else {
-      return current_page;
+      return currentPage;
     }
   }
 });
 Template.body.helpers({
   param: function() {
-    var parameters;
+    var parameters, id;
     id = FlowRouter.getParam("id");
     if (typeof id == undefined) {
       return "none";
