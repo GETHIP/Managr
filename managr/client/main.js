77983b688d11f16f788ed26eee54cd8d8e8fc705
Gerot <Thomas_Gerot@gallup.com>
2016-06-13 11:43:17 -0500

    Fully integrated both single assignment templates

--- managr/client/main.js
+++ managr/client/main.js
@@ -1,9 +1,36 @@
 import { Template } from 'meteor/templating';
-//import { Assignments } from "../collections/assignments.js";
+import { Assignments } from '../collections/assignments.js';
+Template.single.onCreated(function() {
+  Meteor.subscribe('Assignments');
+});
+Template.editSingle.onCreated(function () {
+  Meteor.subscribe('Assignments');
+});
 Template.single.helpers({
-    assignments: function() {
-        var a = Assignments.find().fetch();
-        console.log(a);
-        return a;
-    }
+  assignments: function() {
+    var param = FlowRouter.getParam("id");
+    console.log(param);
+    var obj = new Meteor.Collection.ObjectID(param);
+    console.log(obj);
+    var filter = {"_id":obj}
+    console.log(filter);
+    var a = Assignments.find(filter);
+    console.log(a);
+    console.log(a.fetch());
+    return a;
+  }
+});
+Template.editSingle.helpers({
+  assignments: function() {
+    var param = FlowRouter.getParam("id");
+    console.log(param);
+    var obj = new Meteor.Collection.ObjectID(param);
+    console.log(obj);
+    var filter = {"_id":obj}
+    console.log(filter);
+    var a = Assignments.find(filter);
+    console.log(a);
+    console.log(a.fetch());
+    return a;
+  }
 });
--- managr/collections/assignments.js
+++ managr/collections/assignments.js
@@ -1,5 +1,5 @@
 import { Mongo } from 'meteor/mongo';
-Assignments = new Mongo.Collection('Assignments');
+export const Assignments = new Mongo.Collection('Assignments');
 AssignmentSchema = new SimpleSchema({
     title: {
         type: String,
@@ -40,6 +40,5 @@ AssignmentSchema = new SimpleSchema({
     }
 });
 Assignments.attachSchema(AssignmentSchema);
-/*
-[{title:"Bubble Sort Algorithm",description:"Write a Java algorithm that sorts random elements using the bubble sort method.",dueDate:new Date(),assigner:"Zach Merril",assignedStudents:[0,3,7,15,37,86],dateAssigned:new Date(),pointsPossible:100},{title:"Radix Sort Algorithm",description:"Write a Java algorithm that sorts random elements using the radix sort method",dueDate:new Date(),assigner:"James Getrost",assignedStudents:[0,1,2,3,4,5,6,7,10],dateAssigned:new Date(),pointsPossible:140},{title:"Navigation Bar Design",description:"Create a simple navigation bar with at least 5 links.",dueDate:new Date(),assigner:"Melanie Powell",assignedStudents:[2,4,6,9,12,15,27,29,30,37],dateAssigned:new Date(),pointsPossible:65}]
-*/
+
+// db.Assignments.insert([{title:"Bubble Sort Algorithm",description:"Write a Java algorithm that sorts random elements using the bubble sort method.",dueDate:new Date(),assigner:"Zach Merril",assignedStudents:[0,3,7,15,37,86],dateAssigned:new Date(),pointsPossible:100},{title:"Radix Sort Algorithm",description:"Write a Java algorithm that sorts random elements using the radix sort method",dueDate:new Date(),assigner:"James Getrost",assignedStudents:[0,1,2,3,4,5,6,7,10],dateAssigned:new Date(),pointsPossible:140},{title:"Navigation Bar Design",description:"Create a simple navigation bar with at least 5 links.",dueDate:new Date(),assigner:"Melanie Powell",assignedStudents:[2,4,6,9,12,15,27,29,30,37],dateAssigned:new Date(),pointsPossible:65}]);
