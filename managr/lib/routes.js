aa23632661e51b5bbce6791d211bf2cfb69332d3
Wedergren <Dash_Wedergren@gallup.com>
2016-06-13 08:40:32 -0500

    Mongo now works

---
+++ managr/lib/routes.js
@@ -0,0 +1,7 @@
+FlowRouter.route('/attendance', {
+  name: 'attendance.show',
+  action(params, queryParams) {
+    console.log("Looking at a list?");
+  }
+//  BlazeLayout.render("main", {content: "Login"});
+});
--- managr/server/main.js
+++ managr/server/main.js
@@ -1,5 +1,8 @@
 import { Meteor } from 'meteor/meteor';
+//import { Student } from '../collections/student.js'
+//import '../collections/student.js'
 
 Meteor.startup(() => {
   // code to run on server at startup
+  Student.insert({"name":"bill","profilePicture":"http://","age":5,"strengths":"none","description":"tall","grade":4,"attendance":"three","assignments":"none","school":"West Dodge"});
 });
