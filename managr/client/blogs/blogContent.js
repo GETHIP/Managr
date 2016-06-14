d7a1d6d7145dc52cbadd7ae2f0741082675cc33d
Gideon <Jacob_Gideon@gallup.com>
2016-06-14 15:52:15 -0500

    Commit to merge to master

--- managr/client/blogs/blogContent.js
+++ managr/client/blogs/blogContent.js
@@ -1,7 +1,7 @@
 Template.blogContent.helpers({
   //Returns Posts by Jim for non-logins
       publicPosts: function(){
-        return Posts.find(authorId: {$eq: 1}); //Set ID to Jim's Id (Always Public Posts)
+        return Posts.find({authorId: {$eq: 1}}); //Set ID to Jim's Id (Always Public Posts)
       },
   //Returns all Blog Posts
       allPosts: function(){
