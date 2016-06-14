3aa2fddecaa441fbec5b1fb1a7a8231ca61b1464
Rogers <MaryClare_Rogers@gallup.com>
2016-06-14 15:02:00 -0500

    added page to insert test data

--- managr/server/main.js
+++ managr/server/main.js
@@ -1,8 +1,20 @@
 import { Meteor } from 'meteor/meteor';
+import { Posts } from '../collections/blogPosts.js';
 
 Meteor.startup(() => {
   // code to run on server at startup
   Meteor.publish("Comments", function(){
     return Comments.find();
   });
+  Posts.allow({
+    'insert': function(userId, doc) {
+      true;
+    }
+  });
+  Meteor.methods({
+    'insertPost':function(post) {
+      Posts.insert(post);
+      console.log(Posts.find().fetch());
+    }
+  })
 });
