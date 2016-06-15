8539f168d1efde8ae6ac71b9a39c401a8f3f2869
Yuan <Eddie_Yuan@gallup.com>
2016-06-15 16:18:32 -0500

    Removed deprecated files in partials. Fixed multiple-templates errors.

--- managr/lib/routes.js
+++ managr/lib/routes.js
@@ -1,7 +1,7 @@
 FlowRouter.route('/', {
 	name: 'home',
 	action() {
-		BlazeLayout.render('navbar', {main: 'home'})
+  BlazeLayout.render('blogMain');
 	}
 })
 
@@ -9,7 +9,7 @@ FlowRouter.route('/blogs/:blog_id', {
 	name: 'blogs',
 	action : function(params) {
     console.log("Individual blog post! ", FlowRouter.getParam("blog_id"));
-		BlazeLayout.render('blogsMain');
+		BlazeLayout.render('blogMain');
 	}
 })
 FlowRouter.route('/blog', {
