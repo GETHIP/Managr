9a4e48ad97c5d97166484c99c8b633ecb7003343
Gideon <Jacob_Gideon@gallup.com>
2016-06-15 13:14:15 -0500

    Added basic helpers for sidebar

--- managr/client/blogs/sideNav.js
+++ managr/client/blogs/sideNav.js
@@ -1,1 +1,19 @@
-//Insert helpers for sideNav template here...
+import { Comments } from '../collections/comments.js'
+
+var commentDisplays = 5;
+var postDisplays = 5;
+
+Template.sideNav.helpers({
+  months: function(){
+    //Return months with posts from mongo
+  },
+  recentComments: function(){
+    var comments = [];
+  return comments = Comments.find().sort({Date:1}).slice(0,commentDisplays-1);
+  },
+  recentPosts: function(){
+    var posts = [];
+    return comments = Posts.find().sort({Date:1}).slice(0,commentDisplays-1);
+  },
+  }
+});
