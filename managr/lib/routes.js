75e034d99f9175338e52168a2e893817080edf95
Wingerter <Ben_Wingerter@gallup.com>
2016-06-17 13:55:10 -0500

    Fixed Rohans bad code

--- managr/lib/routes.js
+++ managr/lib/routes.js
@@ -1,11 +1,3 @@
-FlowRouter.route('/attendance', {
-  name: 'attendance.show',
-  action: function(params, queryParams) {
-    console.log("Looking at a list?");
-  }
-//  BlazeLayout.render("main", {content: "Login"});
-});
-
 FlowRouter.route("/profile/:id", {
   action: function(params, queryParams){
     BlazeLayout.render("Profile", {body: "aboutme", attendance: "attendance", editAboutMe: "editAboutMe", assignments: "assignments"});
@@ -16,4 +8,10 @@ FlowRouter.route("/profile/edit/:id", {
   action: function(parmas, queryParams){
     BlazeLayout.render("Profile", {body: "profileEdit", attendance: "attendance", assignments: "assignments"});
   }
+});
+
+FlowRouter.route("/attendance", {
+    action: function(params, queryParams){
+        BlazeLayout.render("attendanceUpdate", {updateAttendance: "updateAttendance"})
+    }
 });
