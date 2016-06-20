d9422415aa31ec60812c153853bf2b72af7d7afb
Dash Wedergren <dashwedergren@gmail.com>
2016-06-20 08:42:08 -0500

    Attendance autofill

--- managr/client/main.js
+++ managr/client/main.js
@@ -28,8 +28,10 @@ Template.attendance.onCreated(function(){
 
 Template.attendance.helpers({
     attendance: function(){
-      let userId = FlowRouter.getParam("id");
-      return attendance = Student.findOne({"_id": userId}).attendance;
+        let userId = FlowRouter.getParam("id");
+        let attendance = [];
+        attendance = Student.findOne({"_id": userId}).attendance;
+        return attendance;
     }
 });
 
@@ -108,23 +110,50 @@ Template.profileEdit.helpers({
     }
 });
 
+Template.attendanceUpdate.onCreated(function(){
+    var self = this;
+    self.autorun(function(){
+      self.subscribe('Student');
+    });
+});
+
 Template.attendanceUpdate.events({
     "submit .attendanceUpdate"(event){
       event.preventDefault();
-      console.log("form submitted")
       let userId = FlowRouter.getParam("id");
       let data = [];
       for(i=1;i<13;i++){
-        let week = event.target["week"+i];
-        week = week.value;
-        if(week === "True"){
-            data.push(true);
-        }else{
-            data.push(false);
-        }
+          let week = event.target["week"+i];
+          let weeks = week.value;
+          if(weeks === "True" || weeks === true){
+              data.push(true);
+          }
+          if(weeks === "False" || weeks === false){
+              data.push(false);
+          }
       }
       console.log(data);
       Student.update({_id: userId}, {$set: {attendance: data}});
       window.location = "/profile/" + FlowRouter.getParam("id");
     }
 });
+
+var wordNumbers = ["zero","one","two","three","four","five","six","seven","eight","nine","ten","eleven","twelve"]
+Template.attendanceUpdate.helpers({
+    attendance: function(){
+        let userId = FlowRouter.getParam("id");
+        let attendanceBoolean = Student.findOne({"_id": userId}).attendance;
+        let attendance = {};
+        for(i=1;i<13;i++){
+            if(attendanceBoolean[i-1] === true){
+                attendance[wordNumbers[i] + "one"] = "selected";
+                attendance[wordNumbers[i] + "two"] = "";
+            }else{
+                attendance[wordNumbers[i] + "one"] = "";
+                attendance[wordNumbers[i] + "two"] = "selected";
+            }
+        };
+        console.log(attendance);
+        return attendance;
+    }
+})
