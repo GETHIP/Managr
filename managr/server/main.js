4179b61a20f4f503a1fcca679ad983ade9a5895f
Knaak <Cooper_Knaak@gallup.com>
2016-06-21 16:27:06 -0500

    Added jim user. Test blog posts now alternate between being posted by jim and being posted by instructor.

--- managr/server/main.js
+++ managr/server/main.js
@@ -34,9 +34,51 @@ Meteor.startup(() => {
          }})
 
     },
+	'testCreatePosts': function() {
+		var jimId = Meteor.users.findOne({username: "jim"})._id;
+		var instructorId = Meteor.users.findOne({username: "instructor"})._id;
+		
+		var i = 0;
+		var dates = [
+			new Date(2016, 1, 1),
+			new Date(2016, 2, 1),
+			new Date(2016, 3, 1),
+			new Date(2015, 1, 1),
+			new Date(2015, 2, 1),
+			new Date(2015, 3, 1),
+			new Date(2014, 1, 1),
+			new Date(2014, 2, 1),
+			new Date(2014, 3, 1),
+			new Date(2013, 1, 1)
+		];
+		for (i = 1; i <= 10; i++) {
+			var id = jimId;
+			if (i % 2 == 0) {
+				id = instructorId;
+			}
+			Meteor.call('insertPost', {
+				title: "Title " + i,
+				text: "Text of the blog post.\n\n\n\nEnd of blog post.",
+				authorId: id,
+				date: dates[i - 1],
+				comments: [
+					{
+						text: "Comment.",
+						authorId: "otherId",
+						date: dates[i - 1]
+					}
+				]
+			});
+		}
+		
+	},
 	'testCreateUsers': function() {
 		var adminId = Accounts.createUser({
 			username: "instructor",
+			password: "password",
+		});
+		var jimId = Accounts.createUser({
+			username: "jim",
 			password: "password",
 		});
 		var studentId = Accounts.createUser({
@@ -44,6 +86,7 @@ Meteor.startup(() => {
 			password: "password",
 		});
 		Roles.addUsersToRoles(adminId, 'instructor');
+		Roles.addUsersToRoles(jimId, 'instructor');
 		Roles.addUsersToRoles(studentId, 'student');
 		/*
 		Meteor.users.find().forEach(function(user) {
