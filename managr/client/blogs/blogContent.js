311b21c7fb0373c9402ca00011a1c67cc10c9173
Yuan <Eddie_Yuan@gallup.com>
2016-06-22 11:05:58 -0500

    Added most recent TestData.

--- managr/client/blogs/blogContent.js
+++ managr/client/blogs/blogContent.js
@@ -1,27 +1,30 @@
 import { Posts } from '../../collections/blogPosts.js';
 
+export function formatDatesOfPosts(posts){
+  var newPosts = [];
+  var newDate;
+  var i;
+  for (i = 0; i < posts.length; i++){
+    newDate = moment(posts[i].date);
+    var formattedDate = moment(newDate).format("M/D/YY");
+
+    newPosts.push({
+      date: formattedDate,
+      title: posts[i].title,
+      text: posts[i].text,
+      authorId: posts[i].authorId,
+      comments: posts[i].comments,
+    });
+  }
+}
+
 Template.blogContent.helpers({
   //Returns Posts by Jim for non-logins
       publicPosts: function() {
         return Posts.find({authorId: {$eq: 1}}); //Set ID to Jim's Id (Always Public Posts)
       },
   //Returns all Blog Posts
-      allPosts: function(){
-        var posts = Posts.find().fetch();
-        var newPosts = [];
-        var newDate;
-        var i;
-        for (i = 0; i < posts.length; i++){
-          newDate = moment(posts[i].date);
-          moment(newDate).format("dddd, MMMM Do YYYY, h:mm:ss a");
-          newPosts.push({
-            date: newDate,
-            title: posts[i].title,
-            text: posts[i].text,
-            authorId: posts[i].authorId,
-            comments: posts[i].comments,
-          });
-        }
-        return newPosts;
+      allPosts: function(){  
+        return formatDatesOfPosts(Posts.find().fetch());
       }
 });
