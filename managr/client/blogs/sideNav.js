d66e047bb1ba61d33f7444e0eb0df524dbd46c3b
Rogers <MaryClare_Rogers@gallup.com>
2016-06-23 10:39:34 -0500

    WiP: Archive functionality for side nav bar.

--- managr/client/blogs/sideNav.js
+++ managr/client/blogs/sideNav.js
@@ -11,6 +11,12 @@ var postDisplays = 5;
 
 Template.sideNav.helpers({
   months: function(){
+    var posts = Posts.find().fetch();
+    var archives = [];
+    var i = 0;
+    for (i = 0; i < posts.length; i++) {
+
+    }
     //Return months with posts from mongo
   },
   recentComments: function(){
@@ -20,6 +26,9 @@ Template.sideNav.helpers({
   recentPosts: function(){
 
     return Posts.find().fetch().slice(0, postDisplays);
+  },
+  archives: function(){
+    Posts.find().fetch()
   },
 });
 
