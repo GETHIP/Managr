701259ae8d68f3f800d11d8ce1828cd4cde01cc3
Jcbgid <jgid261@hotmail.com>
2016-06-29 14:54:06 -0500

    Added title field

--- managr/client/blogs/createField.js
+++ managr/client/blogs/createField.js
@@ -3,13 +3,13 @@ Template.createField.events({
   'submit .postCreate'(){
 
     var postObject = {
-      title: "Post Title",
+      title: event.target.postTitle.value,
       text: event.target.create.value,
       authorId: "12165213541684",
       comments: [],
       date: new Date()
     }
-
+      console.log(event.target.postTitle.value);
       Meteor.call('insertPost', postObject);
     }
 
