6acd38ae89c189ecc6684a3b212b1ade33759d96
Jcbgid <jgid261@hotmail.com>
2016-07-06 15:03:00 -0500

    Made it work

--- managr/client/partials/tabs.js
+++ managr/client/partials/tabs.js
@@ -14,5 +14,25 @@ Template.tabs.events({
   },
   'click .html':function(event) {
     Template.instance().useWYSIWYG.set(false);
+  },
+  'submit .postCreate':function(){
+
+    if (Template.instance().useWYSIWYG.get()) {
+      var editor = document.getElementById('editor');
+      console.log("Editor: " + editor.innerHTML);
+    } else {
+      var editor = document.getElementById('plainTextEditor');
+      console.log("HTML: " + editor);
+    }
+    /*
+    var postObject = {
+      title: event.target.postTitle.value,
+      text: event.target.create.value,
+      authorId: "12165213541684",
+      comments: [],
+      date: new Date()
+    }
+    console.log(event.target.postTitle.value);
+    Meteor.call('insertPost', postObject);*/
   }
 });
