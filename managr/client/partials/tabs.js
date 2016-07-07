afa2859dd00874108a4c9a5c283cd3f20e9fc6f2
Jcbgid <jgid261@hotmail.com>
2016-07-07 15:07:52 -0500

    Working Swap

--- managr/client/partials/tabs.js
+++ managr/client/partials/tabs.js
@@ -1,48 +1,54 @@
 Template.tabs.onCreated(function() {
   Template.instance().useWYSIWYG = new ReactiveVar(true);
+  Session.blogPostText = "";
 });
 
 Template.tabs.helpers({
   useWYSIWYG: function() {
  return Template.instance().useWYSIWYG.get()
-  }
-})
+  },
+});
+
+Template.createField.onRendered(function() {
+    document.getElementById('editor').innerHTML = Session.blogPostText;
+});
+
+Template.HTMLField.onRendered(function() {
+    document.getElementById('create').value = Session.blogPostText;
+});
+
+Template.tabs.onRendered(function() {
+});
 
 Template.tabs.events({
+
   'click .wysiwyg':function(event) {
     Template.instance().useWYSIWYG.set(true);
+    Session.blogPostText = document.getElementById('create').value;
   },
   'click .html':function(event) {
     Template.instance().useWYSIWYG.set(false);
+    Session.blogPostText = document.getElementById('editor').innerHTML;
   },
   'submit .postCreate':function(){
 
     if (Template.instance().useWYSIWYG.get()) {
-      var editor = document.getElementById('editor');
         Meteor.call("insertPost",{
           title:"Will it Work" ,
-          text: editor.innerHTML,
+          text: document.getElementById('editor').innerHTML,
           authorId: 12345,
           date: new Date(),
           comments: []
         });
 
     }else{
-      var editor = document.getElementById('plainTextEditor');
-      console.log("HTML: " + editor);
+      Meteor.call("insertPost",{
+        title:"Will it Work" ,
+        text: document.getElementById('create').value,
+        authorId: 12345,
+        date: new Date(),
+        comments: []
+      });
     }
-
-
-
-    /*
-    var postObject = {
-      title: event.target.postTitle.value,
-      text: event.target.create.value,
-      authorId: "12165213541684",
-      comments: [],
-      date: new Date()
-    }
-    console.log(event.target.postTitle.value);
-    Meteor.call('insertPost', postObject);*/
   }
 });
