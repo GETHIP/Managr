ca397fd7d68bc9fa27ba535a49a58680f65c1971
Huang <Jason_Huang@gallup.com>
2016-07-07 16:14:12 -0500

    more things work now

--- managr/client/main.js
+++ managr/client/main.js
@@ -90,6 +90,9 @@ Template.singleAssignment.onCreated(function() {
 Template.studentsAllAssignments.onCreated(function() {
   Meteor.subscribe('Assignments');
 });
+Template.viewAllAssignTable.onCreated(function() {
+  Meteor.subscribe('Assignments');
+});
 // Provides the assignment data to the single template from Assignments collection
 Template.viewAllAssignTable.events({
   'click #newAssignmentBtn'(event){
@@ -167,6 +170,28 @@ Template.studentsAllAssignments.helpers({
                     assigner: obj.assigner,
                     dateAssigned: (obj.dateAssigned.getMonth() + 1) + "/" + obj.dateAssigned.getDate()  + "/" +  obj.dateAssigned.getFullYear(),
                     pointsPossible: obj.pointsPossible,
+                    url: aUrl
+                }
+                list.push(cleanedObj);
+            }
+        }
+        return list;
+    }
+});
+Template.viewAllAssignTable.helpers({
+    assignments: function() {
+        var list, objects, i;
+        list = [];
+        objects = Assignments.find({}).fetch();
+        console.log("Objects: " + objects.length);
+        for (i = 0; i < objects.length; i++) {
+            if (objects.length > 0) {
+                var obj, j, aUrl, cleanedObj;
+                obj = objects[i];
+                aUrl = "./single/admin/" + obj._id.valueOf();
+                // The formatted object to be returned
+                cleanedObj = {
+                    title: obj.title,
                     url: aUrl
                 }
                 list.push(cleanedObj);
