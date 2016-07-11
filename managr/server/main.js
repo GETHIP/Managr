322722a8cdca2a2de1a9dd1be1704f6c6ed43e02
Knaak <Cooper_Knaak@gallup.com>
2016-07-11 11:54:03 -0500

    Hotfix: fixed missing merge conflict.

--- managr/server/main.js
+++ managr/server/main.js
@@ -271,7 +271,6 @@ Meteor.startup(() => {
 			return true;
 		}
 	});
-<<<<<<< HEAD
 
 	if (Student.find().count == 0) {
 		for (var i = Student.find().count(); i < 5; i++) {
@@ -328,65 +327,6 @@ Meteor.startup(() => {
 				"userId": "asd34ai"
 			});
 		}
-
-=======
-/*
-	Student.remove({});
-	Instructor.remove({});
-	for (var i = Student.find().count(); i < 5; i++) {
-		Student.insert({
-			"name": "ben" + i,
-			"profilePicture": "x",
-			"age": 5,
-			"strengths": ['Input', 'Command', 'Restorative', 'Learner', 'Futuristic'],
-			"description": "tall",
-			"grade": '10th',
-			"attendance": [true, false, true, true, false, false, true, true, false,
-				true, true, false
-			],
-			"assignments": [{
-				"name": "Java Work",
-				"dateAssigned": new Date(),
-				"dueDate": new Date(),
-				"possiblePoints": 100,
-				"pointsRecieved": 10,
-				"instructor": "Zach"
-			}, {
-				name: "Java Work",
-				dateAssigned: new Date(),
-				dueDate: new Date(),
-				possiblePoints: 100,
-				pointsRecieved: 10,
-				instructor: "Zach"
-			}],
-			"school": "West Dodge",
-			"email": "ben@ben.com",
-			"getHipYear": 2,
-			"phoneNumber": '4026571179',
-			"parentNames": ['Bill', 'Hillary'],
-			"address": {
-				"street": '3910 s 226th st.',
-				"city": 'Elkhorn',
-				"state": 'Nebraska',
-				"zipCode": 68022
-			},
-			"github": 'Athletesrun',
-			"blog": "http://blogger.com",
-			"tshirtSize": "Small",
-      "ep10": ["Responsibility", "Profitability", "Communication", "Strategic"],
-			"userId": "asdof889a"
-		});
-	}
-	for (var i = Instructor.find().count(); i < 5; i++) {
-		Instructor.insert({
-			"name": "roger" + i,
-			"profilePicture": "x",
-			"strengths": ['Command', 'Relator', 'Fun', 'Cool', 'Nice'],
-			"description": "Teacher",
-			"email": "Teacher@teacher.com",
-			"userId": "asd34ai"
-		});
->>>>>>> master
 	}
 	console.log(Student.findOne({
 		"name": "ben1"
@@ -394,6 +334,6 @@ Meteor.startup(() => {
 	console.log(Instructor.findOne({
 		"name": "roger1"
 	}));
-	*/
+	
 	createDefaultUser();
 });
