6bbd721c13b796e9592083ecda125297b447ae37
Dash Wedergren <dashwedergren@gmail.com>
2016-07-12 12:14:19 -0500

    Navbar

--- managr/client/main.js
+++ managr/client/main.js
@@ -794,37 +794,7 @@ Template.reports.events({
 				    }else{
 				    		data[i][4] = data[i][4].split(" and ");
 					  }
-						console.log(data[i][4]);
-						Meteor.call('addStudent', {username: data[i][3], password: "G3tH1pPr0gram"});
-						var userId = Session.get("userId");
-						console.log(userId)
-				    Student.insert({
-				        "name": data[i][0],
-								"userId": userId,
-				        "school": data[i][1],
-				        "age": data[i][2],
-				        "email": data[i][3],
-				        "parentNames": data[i][4],
-				        "description": data[i][5],
-				        "grade": data[i][6],
-				        "getHipYear": data[i][7],
-				        "phoneNumber": data[i][8],
-				        "blog": data[i][9],
-				        "strengths": [undefined],
-				        "attendance": [false, false, false, false, false, false, false, false, false, false, false, false],
-				        "assignments": [undefined],
-				        "github": "blank",
-				        "tshirtSize": "blank",
-				        "blog": "blank",
-				        "ep10": [undefined],
-				        "picture": "blank",
-				        "address": {
-				            "street": data[i][10],
-				            "zipCode": 68055,
-				            "state": "blank",
-				            "city": "blank"
-				        }
-				    });
+						Meteor.call('addStudent', data[i]);
 				}
 			}, false);
 		}
--- managr/server/main.js
+++ managr/server/main.js
@@ -214,6 +214,21 @@ Meteor.startup(() => {
 			"attendance": [true, false, true, true, false, false, true, true, false,
 				true, true, false
 			],
+			"assignments": [{
+				"name": "Java Work",
+				"dateAssigned": new Date(),
+				"dueDate": new Date(),
+				"possiblePoints": 100,
+				"pointsRecieved": 10,
+				"instructor": "Zach"
+			}, {
+				name: "Java Work",
+				dateAssigned: new Date(),
+				dueDate: new Date(),
+				possiblePoints: 100,
+				pointsRecieved: 10,
+				instructor: "Zach"
+			}],
 			"school": "Mount Michael",
 			"email": "dash_wedergren@gallup.com",
 			"getHipYear": 2,
@@ -236,16 +251,38 @@ Meteor.startup(() => {
 	'createDefaultUser': function() {
 		createDefaultUser();
 	},
-	'addStudent': function(user){
-		console.log(user.username);
-		console.log(user.id);
-		 user.id = Accounts.createUser({
-				username: user.username,
-				password: user.password
+	'addStudent': function(data){
+		 data.id = Accounts.createUser({
+				username: data[3],
+				password: "G3tH1pPr0gram"
 		 });
-		 Roles.addUsersToRoles(user.id, 'Student');
-		 console.log(user.id);
-		 Session.set("userId", user.id);
+		 Roles.addUsersToRoles(data.id, 'student');
+		 Student.insert({
+		 		"name": data[0],
+		 		"userId": data.id,
+		 		"school": data[1],
+		 		"age": data[2],
+		 		"email": data[3],
+		 		"parentNames": data[4],
+		 		"description": data[5],
+		 		"grade": data[6],
+		 		"getHipYear": data[7],
+		 		"phoneNumber": data[8],
+		 		"blog": data[9],
+		 		"strengths": [undefined],
+		 		"attendance": [false, false, false, false, false, false, false, false, false, false, false, false],
+		 		"github": "blank",
+		 		"tshirtSize": "blank",
+		 		"blog": "blank",
+		 		"ep10": [undefined],
+		 		"picture": "blank",
+		 		"address": {
+		 				"street": data[10],
+		 				"zipCode": 68055,
+		 				"state": "blank",
+		 				"city": "blank"
+		 		}
+		 });
 	}
   });
     Meteor.publish('Assignments', function() {
