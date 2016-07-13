a13f5255d998e9c1ee56b535d4fa6f52a521ec66
Knaak <Cooper_Knaak@gallup.com>
2016-07-13 14:15:06 -0500

    Revert "Hotfix: Script tab in editors lets you post again."
    This reverts commit ecdff685e499502cdf653696d38ea7f2e61b1967.

--- managr/client/blogs/createBlogPost.js
+++ managr/client/blogs/createBlogPost.js
@@ -8,9 +8,12 @@ Template.createBlogPost.onCreated(function() {
 
 Template.createBlogPost.events({
 	'submit .postCreate':function(event){
+		console.log("posted")
 		event.preventDefault();
 		var isPublic = Template.instance().publicPost;
+		console.log(Instructor.find().fetch());
 		var authorName = Instructor.findOne({userId: Meteor.user()._id}).name;
+		console.log(authorName);
 		if (document.getElementById('editor') != undefined) {
 			Meteor.call("insertPost",{
 			  title: document.getElementById('createPostTitle').value ,
@@ -21,10 +24,11 @@ Template.createBlogPost.events({
 			  isPublic: isPublic,
 			  authorName: authorName
 			});
-		} else {
+
+		}else{
 		  Meteor.call("insertPost",{
 			title:document.getElementById('createPostTitle').value ,
-			text: document.getElementById('scriptEditor').value,
+			text: document.getElementById('create').value,
 			authorId: Meteor.user()._id,
 			date: new Date(),
 			comments: [],
