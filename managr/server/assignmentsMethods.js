6764e08bc9a7fed54d4e065172095e66ddce35fb
Knaak <Cooper_Knaak@gallup.com>
2016-07-22 10:10:53 -0500

    Moved code in server/main.js to individual files in server folder.

---
+++ managr/server/assignmentsMethods.js
@@ -0,0 +1,95 @@
+import { Meteor } from 'meteor/meteor';
+import { Posts } from '../collections/blogPosts.js';
+import { Comments } from '../collections/comments.js';
+import { Assignments } from '../collections/assignments.js';
+import { Instructor } from '../collections/instructor.js';
+import { Student } from '../collections/student.js';
+import { Drafts } from '../collections/drafts.js';
+
+export function assignmentsMethods() {
+	Meteor.methods({
+		'removeAssignment':function(assignmentId) {
+		 var allStudents = Student.find({}).fetch();
+		 allStudents.forEach(function(student) {
+				 var assignments = student.assignments;
+				 for(var i = 0; i < assignments.length; i++) {
+						 if(assignments[i].assignmentId == assignmentId) {
+								 assignments.splice(i, 1);
+								 Student.update({_id: student._id},
+								 {
+										 $set: {assignments: assignments}
+								 });
+								 break;
+						 }
+				 }
+		 });
+		 Assignments.remove(assignmentId);
+		},
+		'submitAssignment':function(assignmentId) {
+		 var student = Student.findOne({userId: Meteor.user()._id});
+		 var studentAssignments = student.assignments;
+		 for(var i = 0; i < studentAssignments.length; i++) {
+				 if(studentAssignments[i].assignmentId == assignmentId) {
+						 studentAssignments[i].completed = true;
+						 var successful = Student.update({userId: Meteor.user()._id},
+						 {
+								 $set: {assignments: studentAssignments}
+						 });
+						 break;
+				 }
+		 }
+		},
+		'createAssignment':function(title, description, dueDate, pointsPossible) {
+		 var assignmentId = Assignments.insert({
+		title: title,
+		description: description,
+		dueDate: dueDate,
+		assigner: Instructor.findOne({userId: Meteor.user()._id}).name,
+		dateAssigned: new Date(),
+		pointsPossible: pointsPossible
+		});
+		 Meteor.call("addEmptyAssignmentToAllStudents", assignmentId);
+		},
+		'addEmptyAssignmentToAllStudents':function(assignmentId) {
+		 //A default template for a grade that has no score but must get added to the students
+		var emptyAssignment = {
+		assignmentId: assignmentId,
+		pointsReceived: -1,
+		completed: false
+		};
+
+		var allStudents = Student.find({}).fetch();
+		if(allStudents.length > 0) {
+		for(var i = 0; i < allStudents.length; i++) {
+		 var assignments = allStudents[i].assignments;
+		 if(assignments == undefined) {
+		   assignments = [];
+		 }
+		 assignments.push(emptyAssignment);
+		 Student.update({_id: allStudents[i]._id},
+		 {
+		   $set: {assignments: assignments}
+		 });
+		}
+		}
+		},
+		'updateAssignment':function(assignmentId, title, description, dueDate, pointsPossible) {
+		 var assignment = Assignments.findOne({_id: assignmentId});
+		 var assigner = Instructor.findOne({userId: Meteor.user()._id}).name;
+
+		Assignments.update({
+		_id:assignment._id
+		},
+		{
+		$set: {
+		 title: title,
+		 description: description,
+		 dueDate: dueDate,
+		 assigner: assigner,
+		 dateAssigned: new Date(),
+		 pointsPossible: pointsPossible
+		}
+		});
+		}
+	});
+}
---
+++ managr/server/profilesMethods.js
@@ -0,0 +1,44 @@
+import { Meteor } from 'meteor/meteor';
+import { Posts } from '../collections/blogPosts.js';
+import { Comments } from '../collections/comments.js';
+import { Assignments } from '../collections/assignments.js';
+import { Instructor } from '../collections/instructor.js';
+import { Student } from '../collections/student.js';
+import { Drafts } from '../collections/drafts.js';
+export function profilesMethods() {
+	Meteor.methods({
+		'addStudent': function(data){
+			 data.id = Accounts.createUser({
+					username: data[3],
+					password: "G3tH1pPr0gram"
+			 });
+			 Roles.addUsersToRoles(data.id, 'Student');
+			 Student.insert({
+							"name": data[0],
+							"userId": data.id,
+							"school": data[1],
+							"age": data[2],
+							"email": data[3],
+							"parentNames": data[4],
+							"description": data[5],
+							"grade": data[6],
+							"getHipYear": data[7],
+							"phoneNumber": data[8],
+							"blog": data[9],
+							"strengths": [undefined],
+							"attendance": [false, false, false, false, false, false, false, false, false, false, false, false],
+							"github": "blank",
+							"tshirtSize": "blank",
+							"blog": "blank",
+							"ep10": [undefined],
+							"picture": "blank",
+							"address": {
+									"street": data[10],
+									"zipCode": 68055,
+									"state": "blank",
+									"city": "blank"
+							}
+			 });
+		 },
+	});
+}
