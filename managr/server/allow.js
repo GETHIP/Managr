f359a21680511da728aa4d73f308b53e1f1327ae
Knaak <Cooper_Knaak@gallup.com>
2016-07-25 11:42:55 -0500

    Restricted permissions of all collections on the client side. All manipulation of the database must occur on the server via Meteor.methods.

--- managr/server/allow.js
+++ managr/server/allow.js
@@ -1,6 +1,6 @@
 import { Meteor } from 'meteor/meteor';
 import { Posts } from '../collections/blogPosts.js';
-import { Comments } from '../collections/comments.js';
+// import { Comments } from '../collections/comments.js';
 import { Assignments } from '../collections/assignments.js';
 import { Instructor } from '../collections/instructor.js';
 import { Student } from '../collections/student.js';
@@ -10,37 +10,68 @@ import { isStudent, isInstructor, userIsValid, currentUserOrInstructor, nameOfUs
 
 export function allowAll() {
 
-	Posts.allow({
+	/*
+	 *	We don't allow ANY manipulation of the database on the client.
+	 *	ALL inserts / updates / removes must happen via Meteor.methods,
+	 *	which all have permissions checks.
+	 */
+	Assignments.allow({
 		'insert': function(userId, doc) {
-			if (Meteor.user()._id == null) {
-				return false;
-			}
-			return Meteor.user()._id == userId && isInstructor();
+			return false;
 		},
-		'update': function(userId, doc){
-			true;
+		'update': function(userId, doc) {
+			return false;
 		},
-		'remove': function(userId, doc){
-			true;
+		'remove': function(userId, doc) {
+			return false;
 		}
 	});
-
+	
 	Drafts.allow({
 		'insert': function(userId, doc) {
-			true;
+			return false;
 		},
-		'update': function(userId, doc){
-			true;
+		'update': function(userId, doc) {
+			return false;
 		},
-		'remove': function(userId, doc){
-			true;
+		'remove': function(userId, doc) {
+			return false;
+		}
+	});
+	
+	Student.allow({
+		'insert': function(userId, doc) {
+			return false;
+		},
+		'update': function(userId, doc) {
+			return false;
+		},
+		'remove': function(userId, doc) {
+			return false;
 		}
 	});
 
-	//control update better
-	Student.allow({
-		update: function(userId, doc) {
-			return true;
+	Instructor.allow({
+		'insert': function(userId, doc) {
+			return false;
+		},
+		'update': function(userId, doc) {
+			return false;
+		},
+		'remove': function(userId, doc) {
+			return false;
+		}
+	});
+	
+	Posts.allow({
+		'insert': function(userId, doc) {
+			return false;
+		},
+		'update': function(userId, doc) {
+			return false;
+		},
+		'remove': function(userId, doc) {
+			return false;
 		}
 	});
 
