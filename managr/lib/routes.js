45ebff15abd7ac51f03e163a27e14b4cc7e65ea5
Bledsoe <jared.bledsoe@icloud.com>
2017-06-09 10:46:50 -0500

    Added group button to navbar

--- managr/lib/routes.js
+++ managr/lib/routes.js
@@ -206,18 +206,18 @@ FlowRouter.route('/reports', {
 FlowRouter.route('/groups', {
 	name: 'groups',
 	action: function() {
-		BlazeLayout.render("dashboardLayout", { content: 'groups' });
+		BlazeLayout.render("groupsLayout", { content: 'groups' });
 	}
 });
 FlowRouter.route('/groups/create', {
 	name: 'createGroup',
 	action: function() {
-		BlazeLayout.render("dashboardLayout", {content: 'createGroup'});
+		BlazeLayout.render("groupsLayout", {content: 'createGroup'});
 	}
 });
 FlowRouter.route('/groups/edit/:id', {
 	name: 'editGroup',
 	action: function() {
-		BlazeLayout.render("dashboardLayout", {content: 'editGroup'});
+		BlazeLayout.render("groupsLayout", {content: 'editGroup'});
 	}
 });
