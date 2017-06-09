9080668d13ec58315662e418df2204389103bf28
McWilliams <kieran.j@mcwilliams.cc>
2017-06-09 14:39:48 -0500

    Creating the layout for the groups page

--- managr/collections/groups.js
+++ managr/collections/groups.js
@@ -6,11 +6,17 @@ GroupSchema = new SimpleSchema({
     name: {
         type: String,
         label: "Group Name",
-        optional: false 
+        optional: false
     },
     studentIds: {
         type: [String],
         label: "Student IDs",
+        optional: true
+    },
+    leader: {
+        type: String,
+        label: "Leader",
+        defaultValue: "",
         optional: true
     }
 });
