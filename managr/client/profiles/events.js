c60c52375d4468b6079fe2eb39af2f11bd638d10
Donahue <Hannah_Donahue@gallup.com>
2017-06-13 10:24:41 -0500

    rying to fix edit event page

--- managr/client/profiles/events.js
+++ managr/client/profiles/events.js
@@ -1,19 +1,17 @@
 Template.eventsPage.events({
-<<<<<<< HEAD
+
   'click #calendar': function(event, template) {
     event.preventDefault();
     FlowRouter.go('/events/calendar');
-=======
+},
   'click #createEventButton': function(event, template) {
     event.preventDefault();
     FlowRouter.go('/events/createEvent');
-  }
-});
+  },
 
-Template.eventsPage.events({
   'click #editEventButton': function(event, template) {
     event.preventDefault();
     FlowRouter.go('/editEvent');
->>>>>>> b534821231e537b43c086b987cc6e2fc2862078b
+
   }
 });
