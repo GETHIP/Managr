87a913713b0ef4edaa9f23c49783bef15744cd86
Gong <Owen_Gong@gallup.com>
2017-06-13 10:20:52 -0500

    Edit Event fix

--- managr/client/profiles/events.js
+++ managr/client/profiles/events.js
@@ -1,19 +1,9 @@
 Template.eventsPage.events({
-<<<<<<< HEAD
-  'click #calendar': function(event, template) {
-    event.preventDefault();
-    FlowRouter.go('/events/calendar');
-  }
-});
-
-Template.eventsPage.events({
-=======
 
   'click #calendar': function(event, template) {
     event.preventDefault();
     FlowRouter.go('/events/calendar');
 },
->>>>>>> bc6fadffa45870d684c9452fd959e50257369bb6
   'click #createEventButton': function(event, template) {
     event.preventDefault();
     FlowRouter.go('/events/createEvent');
@@ -22,9 +12,6 @@ Template.eventsPage.events({
   'click #editEventButton': function(event, template) {
     event.preventDefault();
     FlowRouter.go('/editEvent');
-<<<<<<< HEAD
-=======
 
->>>>>>> bc6fadffa45870d684c9452fd959e50257369bb6
   }
-});
+}); 
