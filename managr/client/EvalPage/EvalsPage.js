051f780101709f9d7dbef49b56cb21acba378c9d
Gideon <Jacob_Gideon@gallup.com>
2017-06-13 11:20:20 -0500

    Comment broken code

--- managr/client/EvalPage/EvalsPage.js
+++ managr/client/EvalPage/EvalsPage.js
@@ -4,11 +4,14 @@ import { Eval } from '../../collections/evals.js';
 
 
 
-Template.EvalsPage.events({
-	'click .submit':function(event) {
-    var eAid = Meteor.user()._id;
-    var eId;
-    var comment;
-    Meteor.call("sendEval", eAid, eId, comment);
 
-}});
+
+
+// Template.EvalsPage.events({
+// 	'click .submit':function(event) {
+//     var eAid = Meteor.user()._id;
+//     var eId;
+//     var comment;
+//     Meteor.call("sendEval", eAid, eId, comment);
+//
+// }});
