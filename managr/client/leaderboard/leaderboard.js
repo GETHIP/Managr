0b1588e8badd3e3d0b88a32e47ef98a462fa5f8f
Thakker <Rohan_Thakker@gallup.com>
2017-06-14 13:50:07 -0500

    Stars Finally

--- managr/client/leaderboard/leaderboard.js
+++ managr/client/leaderboard/leaderboard.js
@@ -31,11 +31,12 @@ Template.leaderboard.events({
     event.preventDefault();
     console.log("its clicking");
     var rating = $('#rating').data('userrating');
+    var teamwork = $('#teamwork').data('userrating')
+    var attitude = $('#attitude').data('userrating')
     //var this_student = Student.findOne({"_id"});
     //this_student.rating = rating;
 
     comment = document.getElementById('coSection').value;
-    console.log(comment);
     eaId = Meteor.user()._id;
     eId = "testId";
     week = 5;
