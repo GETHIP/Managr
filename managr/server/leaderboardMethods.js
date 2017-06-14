87a24acc79a673e5a5288d0ec39c9110ff8cd7d2
Gideon <Jacob_Gideon@gallup.com>
2017-06-14 11:00:07 -0500

    Method tuning functionality

--- managr/server/leaderboardMethods.js
+++ managr/server/leaderboardMethods.js
@@ -5,12 +5,13 @@ import { Assignments } from '../collections/assignments.js';
 import { Instructor } from '../collections/instructor.js';
 import { Student } from '../collections/student.js';
 import { Drafts } from '../collections/drafts.js';
+import { Eval } from '../collections/evals.js';
 import { isStudent, isInstructor, userIsValid, currentUserOrInstructor, nameOfUser } from '../lib/permissions.js';
 
 export function leaderboardMethods() {
 	Meteor.methods({
 		'sendEval' : function(eAid, eId, comment, current, sList) {
-			//Impmenent Security
+			//Impmenent Security with roles
 			Eval.insert({evaluator: eAid, evaluatee: eId, message: comment, week: current, stars: sList});
 		},
     'removeEval' : function(id){
