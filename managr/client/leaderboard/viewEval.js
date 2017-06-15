88c6af3885dcb7a72b97ebfee454084d3d7a06dc
Pandian <Sid_Pandian@gallup.com>
2017-06-15 15:46:32 -0500

    pull

--- managr/client/leaderboard/viewEval.js
+++ managr/client/leaderboard/viewEval.js
@@ -1,8 +1,8 @@
 import { Student } from '../../collections/student.js';
-import { Eval } from '../../collections/evals.js'
+import { Eval } from '../../collections/eval.js'
 
 Template.viewEval.helpers({
-	evals: function(){
+	eval: function(){
     var data = Eval.find({evaluator: Meteor.user()._id});
   return list;
 
