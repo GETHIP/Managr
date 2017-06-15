acfaa6a844f5c825fa638d5939de4b7a3b12808d
Gideon <Jacob_Gideon@gallup.com>
2017-06-15 16:09:08 -0500

    Update

--- managr/client/leaderboard/viewEval.js
+++ managr/client/leaderboard/viewEval.js
@@ -1,20 +1,12 @@
 import { Student } from '../../collections/student.js';
 import { Eval } from '../../collections/evals.js'
 
-
 Template.viewEval.helpers({
 	allEvals: function(){
-    return Eval.find({evaluator: Instructor.find({userId: Meteor.user()._id})._id});
+    return Eval.find();
   },
 	fullEval: function(){
 		console.log(FlowRouter.getParam("id"));
 		return Eval.find({_id: FlowRouter.getParam("id")});
 	}
 });
-
-// Template.viewEval.events({
-//   'click .viewEval': function(event){
-//     event.preventDefault();
-// 		eId = document.getElementById('eList').value;
-// 	}
-// });
