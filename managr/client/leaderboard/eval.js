1dacb3ae049cd45b63e5a3d61cafbadc72d6f168
Thakker <Rohan_Thakker@gallup.com>
2017-06-19 08:54:54 -0500

    templates

--- managr/client/leaderboard/eval.js
+++ managr/client/leaderboard/eval.js
@@ -1,7 +1,33 @@
+import { Eval } from '../../collections/eval.js'
+
+Template.leaderboard.onCreated(function(){
+  Meteor.subscribe('Eval');
+});
+
 Template.eval.helpers({
 	eval: function(){
     return Eval.find({_id: FlowRouter.getParam("id")});
-  }
+  },
+	effort: function(element){
+		var starRating = Eval.find({evaluatee: element._id}).fetch();
+		console.log(starRating);
+		var starRating = starRating[0].stars;
+		var effort = eval(starRating[0]);
+		console.log(effort)
+		return effort;
+	},
+	attitude: function(element){
+		var starRating = Eval.find({evaluatee: element._id}).fetch();
+		var starRating = starRating[0].stars;
+		var attitude= eval(starRating[1]);
+		return attitude;
+	},
+	teamwork: function(element){
+		var starRating = Eval.find({evaluatee: element._id}).fetch();
+		var starRating = starRating[0].stars;
+		var teamwork = eval(starRating[2]);
+		return teamwork;
+	}
 });
 
 Template.leaderboard.events({
