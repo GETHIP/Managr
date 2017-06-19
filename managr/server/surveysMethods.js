56114a8a03198379b1a2cec02d904d7fdde20142
Guo <Will_Guo@gallup.com>
2017-06-19 13:28:34 -0500

    added stuff to allow.js and surveysMethods.js

---
+++ managr/server/surveysMethods.js
@@ -0,0 +1,25 @@
+import { Meteor } from 'meteor/meteor';
+import { Posts } from '../collections/blogPosts.js';
+import { Comments } from '../collections/comments.js';
+import { Assignments } from '../collections/assignments.js';
+import { Instructor } from '../collections/instructor.js';
+import { Student } from '../collections/student.js';
+import { Drafts } from '../collections/drafts.js';
+import { Events } from '../collections/event.js';
+import { isStudent, isInstructor, userIsValid, currentUserOrInstructor, nameOfUser } from '../lib/permissions.js';
+
+export function eventsMethods() {
+	Meteor.methods({
+    'createNewSurvey': function(eventName, description, date, location) {
+      if(!isInstructor()) {
+        return;
+      }
+			Events.insert({
+	      name: eventName,
+	      description: description,
+				date: date,
+				location: location
+	    });
+    }
+	});
+}
