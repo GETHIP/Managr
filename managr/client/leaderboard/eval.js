1e8f20904356fae3d3c0d0fa1b11c7d78fa2bb55
Gideon <Jacob_Gideon@gallup.com>
2017-06-19 09:49:23 -0500

    Updated

--- managr/client/leaderboard/eval.js
+++ managr/client/leaderboard/eval.js
@@ -1,6 +1,22 @@
+import { Student } from '../../collections/student.js';
+import { Eval } from '../../collections/eval.js'
+import { Instructor } from '../../collections/instructor.js'
+
+Template.eval.onCreated(function(){
+  Meteor.subscribe('Eval');
+    Meteor.subscribe('Student');
+      Meteor.subscribe('Instructor');
+});
+
 Template.eval.helpers({
 	eval: function(){
-    return Eval.find({_id: FlowRouter.getParam("id")});
+		data = Eval.findOne({_id: FlowRouter.getParam("id")});
+		console.log(data);
+		data.fSt = eval(data.stars[0]);
+		data.sSt = eval(data.stars[1]);
+		data.tSt = eval(data.stars[2]);
+		console.log(data.tSt);
+		return data;
   }
 });
 
