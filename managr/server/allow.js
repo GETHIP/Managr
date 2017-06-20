1dbe959b2e5ac46d9070bdab33a9fc9770814007
Guo <Will_Guo@gallup.com>
2017-06-20 11:15:16 -0500

    added allow.js stuff

--- managr/server/allow.js
+++ managr/server/allow.js
@@ -53,17 +53,17 @@ export function allowAll() {
 		}
 	});
 
-	Surveys.allow({
-		'insert': function(userId, doc) {
-			return false;
-		},
-		'update': function(userId, doc) {
-			return false;
-		},
-		'remove': function(userId, doc) {
-			return false;
-		}
-	});
+	// Surveys.allow({
+	// 	'insert': function(userId, doc) {
+	// 		return false;
+	// 	},
+	// 	'update': function(userId, doc) {
+	// 		return false;
+	// 	},
+	// 	'remove': function(userId, doc) {
+	// 		return false;
+	// 	}
+	// });
 
 
 	Groups.allow({
