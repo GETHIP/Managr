fc34fa868a2eb5da819f4bd874bdf6c68bd77cd2
Gong <Owen_Gong@gallup.com>
2017-06-20 13:01:14 -0500

    something

--- managr/client/events/editEvent.js
+++ managr/client/events/editEvent.js
@@ -18,31 +18,31 @@ Template.editEvent.onCreated(function() {
 Template.editEvent.events({
 	'click .editEventButton':function(event){
 		event.preventDefault();
-		var authorName = Instructor.findOne({userId: Meteor.user()._id}).name;
-		if (document.getElementById('editor') != undefined) {
-			if(document.getElementById('createPostTitle').value == "" && document.getElementById('editor').innerHTML == ""){
-				Modal.show("missingFields", "Please enter a title and body text before posting.");
-			} else if(document.getElementById('createPostTitle').value == ""){
-				Modal.show("missingFields", "Please enter a title before posting.");
-			} else if(document.getElementById('editor').innerHTML == ""){
-				Modal.show("missingFields", "Please enter body text before posting.");
-			} else {
-				Meteor.call("updatePost", FlowRouter.getParam("blog_id"), document.getElementById('editor').innerHTML, document.getElementById('createPostTitle').value, isPublic);
-				FlowRouter.go("/blogs/"+FlowRouter.getParam("blog_id"));
-			}
-		} else {
-			if(document.getElementById('createPostTitle').value == "" && document.getElementById('scriptEditor').value == ""){
-				Modal.show("missingFields", "Please enter a title and body text before posting.");
-			} else if(document.getElementById('createPostTitle').value == ""){
-				Modal.show("missingFields", "Please enter a title before posting.");
-			} else if(document.getElementById('scriptEditor').value == ""){
-				Modal.show("missingFields", "Please enter body text before posting.");
-			} else {
-				Meteor.call("updatePost", FlowRouter.getParam("blog_id"), document.getElementById('scriptEditor').value, document.getElementById('createPostTitle').value, isPublic);
-				FlowRouter.go("/blogs/"+FlowRouter.getParam("blog_id"));
-			}
+		// var authorName = Instructor.findOne({userId: Meteor.user()._id}).name;
+		// if (document.getElementById('editor') != undefined) {
+		// 	if(document.getElementById('createPostTitle').value == "" && document.getElementById('editor').innerHTML == ""){
+		// 		Modal.show("missingFields", "Please enter a title and body text before posting.");
+		// 	} else if(document.getElementById('createPostTitle').value == ""){
+		// 		Modal.show("missingFields", "Please enter a title before posting.");
+		// 	} else if(document.getElementById('editor').innerHTML == ""){
+		// 		Modal.show("missingFields", "Please enter body text before posting.");
+		// 	} else {
+		// 		Meteor.call("updatePost", FlowRouter.getParam("blog_id"), document.getElementById('editor').innerHTML, document.getElementById('createPostTitle').value, isPublic);
+		// 		FlowRouter.go("/blogs/"+FlowRouter.getParam("blog_id"));
+		// 	}
+		// } else {
+		// 	if(document.getElementById('createPostTitle').value == "" && document.getElementById('scriptEditor').value == ""){
+		// 		Modal.show("missingFields", "Please enter a title and body text before posting.");
+		// 	} else if(document.getElementById('createPostTitle').value == ""){
+		// 		Modal.show("missingFields", "Please enter a title before posting.");
+		// 	} else if(document.getElementById('scriptEditor').value == ""){
+		// 		Modal.show("missingFields", "Please enter body text before posting.");
+		// 	} else {
+		// 		Meteor.call("updatePost", FlowRouter.getParam("blog_id"), document.getElementById('scriptEditor').value, document.getElementById('createPostTitle').value, isPublic);
+		// 		FlowRouter.go("/blogs/"+FlowRouter.getParam("blog_id"));
+		// 	}
 		}
-	},
+	});
 
 Template.eventsPage.events({
   'click #editEventButton': function(event, template) {
