6224eb4922cf5b4250d900a852a28f8fd7adacdf
Rogers <MaryClare_Rogers@gallup.com>
2017-06-20 08:39:25 -0500

    calendarButton

---
+++ managr/client/profiles/calendar.js
@@ -0,0 +1,18 @@
+Template.calendar.onRendered( () => {
+  $( '#calendarButton' ).fullCalendar({
+    header: { right: 'month,agendaWeek,agendaDay',
+              center: 'title',
+              left: 'today prev,next'},
+
+          eventLimit: true, // for all non-agenda views
+  views: {
+      agenda: {
+          eventLimit: 8 // adjust to 6 only for agendaWeek/agendaDay
+      }
+  },
+  navLinks: true, //can click day/week names to navigate views
+  selectable: true, //ability to select days and time slots
+  selectHelper: true,  //placeholder when you select the time slots
+  allDaySlot: true
+  });
+});
