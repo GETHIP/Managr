90a0f2c86620ae61c78c2b123e4174e41bfe1f16
Gideon <Jacob_Gideon@gallup.com>
2017-06-21 12:32:21 -0500

    Update

--- managr/client/leaderboard/viewEval.js
+++ managr/client/leaderboard/viewEval.js
@@ -1,14 +1,29 @@
-import { Eval } from '../../collections/eval.js'
+import { Student } from '../../collections/student.js';
+import { Eval } from '../../collections/eval.js';
+import { Instructor } from '../../collections/instructor.js';
 
 Template.viewEval.onCreated(function(){
-	console.log("it subbing");
   Meteor.subscribe('Eval');
+  Meteor.subscribe('Student');
+  Meteor.subscribe('Instructor');
 });
 
 
 Template.viewEval.helpers({
 	data: function(){
 		console.log(Eval.find({}).fetch());
-    return Eval.find();
+		data = Eval.find().fetch();
+    for(var i = 0; i < data.length; i++){
+		data[i].name = Student.findOne({_id: data[i].evaluatee}).name;
+    data[i].instructor = Instructor.findOne({_id: data[i].evaluator}).name;
+    console.log(data[i].stars);
+		data[i].effort = data[i].stars[0];
+		data[i].att = data[i].stars[1];
+		data[i].team = data[i].stars[2];
+		data[i].tech = data[i].stars[3];
+
+  }
+  console.log(data);
+  return data;
   }
 });
