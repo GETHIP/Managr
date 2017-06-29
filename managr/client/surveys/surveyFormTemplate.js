81c819588b374c5014f4234527d789b7e050f59c
Nguyen <Nick_Nguyen@gallup.com>
2017-06-29 09:44:06 -0500

    Student

--- managr/client/surveys/surveyFormTemplate.js
+++ managr/client/surveys/surveyFormTemplate.js
@@ -7,6 +7,7 @@ Template.newSurvey.onCreated(function() {
     Meteor.subscribe("Student");
     Meteor.subscribe("Surveys");
     Meteor.subscribe("Questions");
+    Meteor.subscribe("Groups");
 });
 
 Template.newSurvey.events({
@@ -36,3 +37,32 @@ Template.newSurvey.events({
 //   }
 //
 // });
+Template.surveyFormTemplate.helpers({
+    groups: function() {
+        var allGroups = Groups.find({}).fetch();
+        var formattedGroups = [];
+        for (var i = 0; i < allGroups.length; i++) {
+            var formattedGroup = {
+                name: allGroups[i].name,
+                id: allGroups[i]._id
+            }
+            formattedGroups.push(formattedGroup);
+        }
+        return formattedGroups;
+    },
+    students: function() {
+        var allStudents = Student.find({}).fetch();
+        var formattedStudents = [];
+        for (var i = 0; i < allStudents.length; i++) {
+            var student = allStudents[i];
+            var formattedStudent = {
+                name: student.name,
+                id: student._id
+            }
+            formattedStudents.push(formattedStudent);
+        }
+        return formattedStudents;
+    }
+});
+
+//May need to add more from newAssignment.js for group functionality
