73446e22d5c4b96766b6802c70c6279be78360d7
Longo <Cate_Longo@gallup.com>
2017-07-05 16:20:35 -0500

    final modal

--- managr/client/leaderboard/milestonemodal.js
+++ managr/client/leaderboard/milestonemodal.js
@@ -1,3 +1,14 @@
+Template.deleteMilestoneModal.events({
+
+	"click #confirmDeleteEditEval"(event) {
+		event.preventDefault();
+		var evalId = FlowRouter.getParam("id");
+		Meteor.call('removeMilestone', milestoneId);
+		FlowRouter.go("/dashboard");
+	}
+
+});
+
 /*Template.deleteMilestoneModal.events({
 
 	"click #deleteMilestoneButton"(event) {
