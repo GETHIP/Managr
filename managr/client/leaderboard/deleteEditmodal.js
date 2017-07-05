77985bf3ec386bf03a8cff670f8b3c81804fd019
Longo <Cate_Longo@gallup.com>
2017-07-05 13:28:25 -0500

    modal 2

---
+++ managr/client/leaderboard/deleteEditmodal.js
@@ -0,0 +1,10 @@
+Template.deleteEditModal.events({
+
+	"click #confirmDeleteEditEval"(event) {
+		event.preventDefault();
+		var evalId = FlowRouter.getParam("id");
+		Meteor.call('removeEval', evalId);
+		FlowRouter.go("/editEval");
+	}
+
+});
